7cce12666f3567a7c1144c2916ba11206ab33481
M1croZavr <40316726+M1croZavr@users.noreply.github.com>
2022-03-14 22:12:13 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import onedimensionaloptimization.optimize as optimize
-import twovarextremas
-import onedimensionaloptimization
-
-
-# print(onedimensionaloptimization.)
-print(twovarextremas.find_extrema)
