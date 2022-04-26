a9e8cf3c5a9c4f0c6ae7646a4969066bf17c1016
= <tlashukov@gmail.com>
2022-04-26 22:37:39 +0300

    All methods and visualization implementation first commit.

--- interiorpoint/optimize.py
+++ interiorpoint/optimize.py
@@ -2,11 +2,11 @@ from twovarextremas.utils_twovarextremas import preproc_fun
 import numpy as np
 import sympy
 from scipy.optimize import minimize
-from .plotting import draw_level_lines, draw_lines
+from .plotting import draw_level_lines, draw_lines, draw_feasible_polygon
 
 
 def newton_dual(fun, g, x_init, plot=True):
-    """Optimizes function of 1 variable by golden section method.
+    """Optimization of a function with equality-type constraints.
 
     Positional arguments:
     fun -- str, function in analytic form
@@ -14,7 +14,7 @@ def newton_dual(fun, g, x_init, plot=True):
     x_init -- numpy.ndarray, initial approximation
 
     Keyword arguments:
-    plot -- Draw plot(default=False)
+    plot -- bool, Draw plot(default=False)
     """
     fun = preproc_fun(fun)
     variables = list(fun.atoms(sympy.Symbol))
@@ -56,8 +56,11 @@ def newton_dual(fun, g, x_init, plot=True):
             for x in x_history:
                 points.append((x, float(fun.subs(variables[0], x[0]))))
             draw_lines(X, Y, points, variables)
-
-    print(f'f(X): {minimize_result["fun"]} | X: {dict(zip(variables, minimize_result["x"]))} | Message: {minimize_result["message"]}')
+    res_f = minimize_result["fun"]
+    res_x = dict(zip(variables, minimize_result["x"]))
+    message = minimize_result["message"]
+    print(f'f(X): {res_f} | X: {res_x} | Message: {message}')
+    return res_f, res_x
     # b = []
     # A = []
     # for g_i in g:
@@ -79,16 +82,23 @@ def newton_dual(fun, g, x_init, plot=True):
     # print(f'f(x): {minimize_result["fun"]} | x: {minimize_result["x"]} | Message: {minimize_result["message"]}')
 
 
-def log_barriers(fun, g, x_init, t, tol_barrier=1e-5, tol_newton=1e-5, maxiter=200, plot=True):
-    """Optimizes function of 1 variable by golden section method.
+def log_barriers(fun, g, x_init, t=0.1, c=2., tol_barrier=1e-4, tol_newton=1e-4, maxiter=50, plot=True):
+    """Optimization of a Function with Inequality Constraints by the Logarithmic Barrier Method
+    Be careful to have doubly differentiable fun to properly run function.
+    Parameter x_init must satisfy constraints to better converge on the right solution.
 
     Positional arguments:
     fun -- str, function in analytic form
-    g -- sequence(list, tuple ...) of str, sequence of inequality/equality type constraints functions in analytic form
+    g -- sequence(list, tuple ...) of str, sequence of inequality type constraints functions in analytic form
     x_init -- numpy.ndarray, initial approximation
 
     Keyword arguments:
-    plot -- Draw plot(default=False)
+    t -- float, initialization for logarithmic barrier coefficient(default=0.1)
+    c -- float, multiplier of t each iteration of barrier method(default=2.)
+    tol_barrier -- float, precision for logarithmic barrier part of solution(default=1e-4)
+    tol_newton -- float, precision for newton gradient method part of solution(default=1e-4)
+    max_iter -- int, number of iterations in newton gradient method(default=200)
+    plot -- bool, Draw plot(default=False)
     """
     x = x_init
     for i in range(len(g)):
@@ -101,25 +111,128 @@ def log_barriers(fun, g, x_init, t, tol_barrier=1e-5, tol_newton=1e-5, maxiter=2
         elif '<=' in g[i]:
             left, right = g[i].split('<=')
             g[i] = f'(-({left.strip()}) + {right.strip()})'
-    # Пересчитываем гессиану с новым t
-    z = preproc_fun(f'{fun} - (1 / {t}) * ln({"*".join(g)})')
-    variables = list(z.atoms(sympy.Symbol))
-    grad = sympy.derive_by_array(z, variables)  # Градиенты считает правильно
+    fun_prep = preproc_fun(fun)
+    z = preproc_fun(f'{fun} - (1 / t) * ln({"*".join(g)})')
+    variables = list(fun_prep.atoms(sympy.Symbol))
+    grad = sympy.derive_by_array(z, variables)
     hess = sympy.hessian(z, variables)
-    while len(g) / t > tol_barrier:
+    j = 0
+    x_history = [x]
+    while len(g) / t > tol_barrier and j < maxiter:
         i = 0
         hg = np.ones((len(variables), )).reshape(-1, 1)
         while np.linalg.norm(hg) > tol_newton and i < maxiter:
-            gx = np.array(grad.subs(dict(zip(variables, x))), dtype=np.float64)
-            hx = np.array(hess.subs(dict(zip(variables, x))), dtype=np.float64)
+            gx = np.array(grad.subs(zip(variables, x)).subs({'t': t}), dtype=np.float64)
+            hx = np.array(hess.subs(zip(variables, x)).subs({'t': t}), dtype=np.float64)
             hg = -1 * (np.linalg.inv(hx) @ gx).flatten()
             x = x + hg
+            x_history.append(x)
             i += 1
-        # !!!!
-        t = (1 + 1/(13 * np.sqrt(0.01))) * t
-        z = preproc_fun(f'{fun} - (1 / {t}) * ln({"*".join(g)})')
-        grad = sympy.derive_by_array(z, variables)
-        hess = sympy.hessian(z, variables)
-    return x, variables
+        t = c * t
+        # z = preproc_fun(f'{fun} - (1 / {t}) * ln({"*".join(g)})')
+        # grad = sympy.derive_by_array(z, variables)
+        # hess = sympy.hessian(z, variables)
+        j += 1
+    if j != maxiter:
+        message = 'Optimization terminated successfully'
+    else:
+        message = 'Reached the limit of iterations'
+    fun = preproc_fun(fun)
+    if plot:
+        if len(variables) == 2:
+            X, Y = np.meshgrid(np.linspace(-10, 10, 25), np.linspace(-10, 10, 25))
+            Z = np.array([[float(fun.subs([(variables[0], x), (variables[1], y)]))
+                           for x, y in zip(x_i, y_i)] for x_i, y_i in zip(X, Y)])
+            points = []
+            for x in x_history:
+                points.append(dict(zip(variables, x)))
+            draw_level_lines(X, Y, Z, points, variables)
+            X, Y = np.meshgrid(np.linspace(-10, 10, 250), np.linspace(-10, 10, 250))
+            draw_feasible_polygon(X, Y, g, variables)
+        elif len(variables) == 1:
+            X = np.linspace(-10, 10)
+            Y = np.array([float(fun.subs(variables[0], x_i)) for x_i in X])
+            points = []
+            for x in x_history:
+                points.append((x, float(fun.subs(variables[0], x[0]))))
+            draw_lines(X, Y, points, variables)
+    res_f = fun.subs(zip(variables, x))
+    res_x = dict(zip(variables, x))
+    print(f'f(X): {res_f} | X: {res_x} | Message: {message}')
+    return res_f, res_x
 
+
+def direct_dual_interior_point(fun, g, x_init, plot=True):
+    """Optimization of a function with equality and inequality-type constraints.
+
+    Positional arguments:
+    fun -- str, function in analytic form
+    g -- sequence(list, tuple ...) of str, sequence of equality type constraints functions in analytic form
+    x_init -- numpy.ndarray, initial approximation
+
+    Keyword arguments:
+    plot -- Draw plot(default=False)
+    """
+    fun = preproc_fun(fun)
+    variables = list(fun.atoms(sympy.Symbol))
+    constraints = []
+    for g_i in g:
+        if '>=' in g_i:
+            left, right = tuple(map(lambda x: x.strip(), g_i.split('>=')))
+
+            def constraint(x, left=left, right=right):
+                return sympy.lambdify(variables, preproc_fun(left) - preproc_fun(right))(*x)
+
+            constraints.append({'type': 'ineq', 'fun': constraint})
+
+        elif '<=' in g_i:
+            left, right = tuple(map(lambda x: x.strip(), g_i.split('<=')))
+
+            def constraint(x, left=left, right=right):
+                return sympy.lambdify(variables, -1 * preproc_fun(left) + preproc_fun(right))(*x)
+
+            constraints.append({'type': 'ineq', 'fun': constraint})
+        else:
+            left, right = tuple(map(lambda x: x.strip(), g_i.split('=')))
+
+            def constraint(x, left=left, right=right):
+                return sympy.lambdify(variables, preproc_fun(left) - preproc_fun(right))(*x)
+
+            constraints.append({'type': 'eq', 'fun': constraint})
+
+    f = sympy.lambdify(variables, fun)
+
+    def callback(xk, state):
+        x_history.append(xk)
+
+    x_history = [x_init]
+    minimize_result = minimize(lambda x: f(*x),
+                               x_init,
+                               constraints=constraints,
+                               method='trust-constr',
+                               callback=callback)
+
+    if plot:
+        if len(variables) == 2:
+            X = np.linspace(-10, 10)
+            Y = np.linspace(-10, 10)
+            X, Y = np.meshgrid(X, Y)
+            Z = np.array([[float(fun.subs([(variables[0], x), (variables[1], y)]))
+                           for x, y in zip(x_i, y_i)] for x_i, y_i in zip(X, Y)])
+            points = []
+            for x in x_history:
+                points.append(dict(zip(variables, x)))
+            draw_level_lines(X, Y, Z, points, variables)
+        elif len(variables) == 1:
+            X = np.linspace(-10, 10)
+            Y = np.array([float(fun.subs(variables[0], x_i)) for x_i in X])
+            points = []
+            for x in x_history:
+                points.append((x, float(fun.subs(variables[0], x[0]))))
+            draw_lines(X, Y, points, variables)
+    res_f = minimize_result["fun"]
+    res_x = dict(zip(variables, minimize_result["x"]))
+    message = minimize_result["message"]
+    print(f'f(X): {res_f} | X: {res_x} | Message: {message}')
+    return res_f, res_x
 
