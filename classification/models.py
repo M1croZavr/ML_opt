10396ed64d3bdcf4cef551409fe07533ff3caf67
= <tlashukov@gmail.com>
2022-05-14 17:03:19 +0300

    Polynomial features added, drawing fixes.

--- classification/models.py
+++ classification/models.py
@@ -2,7 +2,7 @@ import numpy as np
 from onedimensionaloptimization import utils_onedimensionaloptimization as uo
 from twovarextremas import plotting_3d
 from sklearn import preprocessing, svm, pipeline
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, patches, lines
 
 
 def sigmoid(wx):
@@ -11,7 +11,7 @@ def sigmoid(wx):
 
 class LogisticRegressionRidge:
     """
-    Logistic regression with Ridge regularization by coefficient C(inverse of lambda)
+    Logistic regression with Ridge regularization by coefficient C(inverse of lambda, reg term = 1 / C)
 
     LogisticRegression fits a linear model with sigmoid function coefficients w = (w1, ..., wp)
     to minimize the log loss between the observed targets in the dataset.
@@ -20,10 +20,12 @@ class LogisticRegressionRidge:
     ----------
     lr -- float, default=0.001
         Learning rate in SGD
-    eps -- float, default=0.00005
+    eps -- float, default=0.00001
         Convergence condition coefficient in SGD
     C -- float, default=1
         Inverse of regularization coefficient
+    degree -- int, default=None
+        Defines maximum degree of generating polynomial features. Leave default to fit without polynomial.
     plot -- bool, default=False
         Draw result plot or not
 
@@ -32,10 +34,16 @@ class LogisticRegressionRidge:
     w -- array, weights of features found by SGD
     ...
     """
-    def __init__(self, lr=0.001, eps=0.00005, C=1, plot=False):
+    def __init__(self, lr=0.001, eps=0.00001, C=1, degree=None, plot=False):
         self.lr = lr
         self.eps = eps
         self.C = C
+        if degree:
+            self.degree = degree
+            self.poly_features = preprocessing.PolynomialFeatures(degree=self.degree)
+        else:
+            self.degree = None
+            self.poly_features = None
         self.plot = plot
         self.w = None
 
@@ -52,7 +60,10 @@ class LogisticRegressionRidge:
         -------
         self -- object, fitted model
         """
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        if self.poly_features:
+            X = self.poly_features.fit_transform(X)
+        else:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
         indices = np.arange(X.shape[0])
         self.w = np.ones((X.shape[1], ))
         w_old = None
@@ -70,13 +81,16 @@ class LogisticRegressionRidge:
         return self
 
     def make_plot(self, X, y, grid_step=.01):
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=(14, 9))
         plt.scatter(X[y == 1, 0], X[y == 1, 1], c='green', marker='+', label='class 1')
         plt.scatter(X[y == 0, 0], X[y == 0, 1], c='red', marker='D', label='class 0')
         plt.xlabel("Feature 1")
         plt.ylabel("Feature 2")
         plt.title('Classification decision plane')
-        plt.legend()
+        legend_elements = [patches.Patch(facecolor='green', edgecolor='green', label='Class 1'),
+                           patches.Patch(facecolor='red', edgecolor='red', label='Class 2'),
+                           patches.Patch(facecolor='black', edgecolor='black', label='Separating plane')]
+        plt.legend(handles=legend_elements)
         x1_min, x1_max = X[:, 0].min() - .1, X[:, 0].max() + .1
         x2_min, x2_max = X[:, 1].min() - .1, X[:, 1].max() + .1
         xx, yy = np.meshgrid(np.arange(x1_min, x1_max, grid_step),
@@ -98,7 +112,10 @@ class LogisticRegressionRidge:
         -------
         y -- array, array of predictions of regression model
         """
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        if self.poly_features:
+            X = self.poly_features.transform(X)
+        else:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
         return sigmoid(np.sum(self.w * X, axis=1))
 
     def predict(self, X, threshold=0.5):
@@ -114,7 +131,10 @@ class LogisticRegressionRidge:
         -------
         y -- array, array of predictions of regression model
         """
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        if self.poly_features:
+            X = self.poly_features.transform(X)
+        else:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
         return (np.sum(self.w * X, axis=1) >= threshold).astype(np.float32)
 
     def __str__(self):
@@ -126,7 +146,7 @@ class LogisticRegressionRidge:
 
 class LogisticRegressionLasso:
     """
-    Logistic regression with Lasso regularization by coefficient C(inverse of lambda)
+    Logistic regression with Lasso regularization by coefficient C(inverse of lambda, reg term = 1 / C)
 
     LogisticRegression fits a linear model with sigmoid function coefficients w = (w1, ..., wp)
     to minimize the log loss between the observed targets in the dataset.
@@ -139,6 +159,8 @@ class LogisticRegressionLasso:
         Convergence condition coefficient in SGD
     C -- float, default=1
         Inverse of regularization coefficient
+    degree -- int, default=None
+        Defines maximum degree of generating polynomial features
     plot -- bool, default=False
         Draw result plot or not
 
@@ -147,10 +169,16 @@ class LogisticRegressionLasso:
     w -- array, weights of features found by SGD
     ...
     """
-    def __init__(self, lr=0.001, eps=0.00005, C=1, plot=False):
+    def __init__(self, lr=0.001, eps=0.00005, C=1, degree=None, plot=False):
         self.lr = lr
         self.eps = eps
         self.C = C
+        if degree:
+            self.degree = degree
+            self.poly_features = preprocessing.PolynomialFeatures(degree=self.degree)
+        else:
+            self.degree = None
+            self.poly_features = None
         self.plot = plot
         self.w = None
 
@@ -167,7 +195,10 @@ class LogisticRegressionLasso:
         -------
         self -- object, fitted model
         """
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        if self.poly_features:
+            X = self.poly_features.fit_transform(X)
+        else:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
         indices = np.arange(X.shape[0])
         self.w = np.ones((X.shape[1], ))
         w_old = None
@@ -191,7 +222,10 @@ class LogisticRegressionLasso:
         plt.xlabel("Feature 1")
         plt.ylabel("Feature 2")
         plt.title('Classification decision plane')
-        plt.legend()
+        legend_elements = [patches.Patch(facecolor='green', edgecolor='green', label='Class 1'),
+                           patches.Patch(facecolor='red', edgecolor='red', label='Class 2'),
+                           patches.Patch(facecolor='black', edgecolor='black', label='Separating plane')]
+        plt.legend(handles=legend_elements)
         x1_min, x1_max = X[:, 0].min() - .1, X[:, 0].max() + .1
         x2_min, x2_max = X[:, 1].min() - .1, X[:, 1].max() + .1
         xx, yy = np.meshgrid(np.arange(x1_min, x1_max, grid_step),
@@ -213,7 +247,10 @@ class LogisticRegressionLasso:
         -------
         y -- array, array of predictions of regression model
         """
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        if self.poly_features:
+            X = self.poly_features.transform(X)
+        else:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
         return sigmoid(np.sum(self.w * X, axis=1))
 
     def predict(self, X, threshold=0.5):
@@ -229,7 +266,10 @@ class LogisticRegressionLasso:
         -------
         y -- array, array of predictions of regression model
         """
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        if self.poly_features:
+            X = self.poly_features.transform(X)
+        else:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
         return (np.sum(self.w * X, axis=1) >= threshold).astype(np.float32)
 
     def __str__(self):
@@ -241,18 +281,20 @@ class LogisticRegressionLasso:
 
 class SupportVectorClassifier:
     """
-    Linear support vector classifier with Ridge and Lasso regularization by coefficient C(inverse of lambda)
+    Support vector classifier with Ridge regularization by coefficient C(inverse of lambda, reg term = 1 / C)
 
-    SupportVectorClassifier fits a linear model with coefficients w = (w1, ..., wp)
+    SupportVectorClassifier fits a model with coefficients w = (w1, ..., wp)
     to minimize the hinge loss between the observed targets in the dataset without using dual optimization problem.
 
     Parameters
     ----------
-    reg -- str, default=Ridge
-        Which regularization to use (ridge or lasso)
     C -- float, default=1
         Inverse of regularization coefficient
-    eps -- float, default=0.0005
+    kernel -- str, default='rbf'
+        Specifies the kernel type to be used in the algorithm. {'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'}.
+    degree -- int, default=3
+        Degree of the polynomial kernel function ('poly').
+    eps -- float, default=0.001
         Convergence tolerance of optimization problem
     plot -- bool, default=False
         Draw result plot or not
@@ -263,25 +305,21 @@ class SupportVectorClassifier:
         Pipeline with support vector classifier model and scaler
     ...
     """
-    def __init__(self, reg='Ridge', C=1, eps=0.0005, plot=False):
-        if reg.lower() == 'ridge':
-            self.reg = 'l2'
-        elif reg.lower() == 'lasso':
-            self.reg = 'l1'
-        else:
-            raise ValueError('Regularization must be one of {\'Ridge\', \'Lasso\'}')
+    def __init__(self, C=1, kernel='rbf', degree=3, eps=0.001, plot=False):
         self.C = C
+        self.kernel=kernel
+        self.degree = degree
         self.eps = eps
         self.pipeline = pipeline.Pipeline([('Scaler', preprocessing.StandardScaler()),
-                                           ('Estimator', svm.LinearSVC(penalty=self.reg,
-                                                                       tol=self.eps,
-                                                                       C=self.C,
-                                                                       dual=False))])
+                                           ('Estimator', svm.SVC(kernel=self.kernel,
+                                                                 tol=self.eps,
+                                                                 C=self.C,
+                                                                 degree=self.degree))])
         self.plot = plot
 
     def fit(self, X, y):
         """
-        Fits linear support vector classifier model.
+        Fits support vector classifier model.
 
         Parameters
         ----------
@@ -298,13 +336,16 @@ class SupportVectorClassifier:
         return self.pipeline
 
     def make_plot(self, X, y, grid_step=.01):
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=(14, 9))
         plt.scatter(X[y == 1, 0], X[y == 1, 1], c='green', marker='+', label='class 1')
         plt.scatter(X[y == 0, 0], X[y == 0, 1], c='red', marker='D', label='class 0')
         plt.xlabel("Feature 1")
         plt.ylabel("Feature 2")
         plt.title('Classification decision plane')
-        plt.legend()
+        legend_elements = [patches.Patch(facecolor='green', edgecolor='green', label='Class 1'),
+                           patches.Patch(facecolor='red', edgecolor='red', label='Class 2'),
+                           patches.Patch(facecolor='black', edgecolor='black', label='Separating plane')]
+        plt.legend(handles=legend_elements)
         x1_min, x1_max = X[:, 0].min() - .1, X[:, 0].max() + .1
         x2_min, x2_max = X[:, 1].min() - .1, X[:, 1].max() + .1
         xx, yy = np.meshgrid(np.arange(x1_min, x1_max, grid_step),
@@ -337,7 +378,7 @@ class SupportVectorClassifier:
 
 class SupportVectorClassifierDual:
     """
-    Linear support vector classifier with Ridge and Lasso regularization by coefficient C(inverse of lambda)
+    Linear support vector classifier with Ridge and Lasso regularization by coefficient C(inverse of lambda, reg term = 1 / C)
 
     SupportVectorClassifier fits a linear model with coefficients w = (w1, ..., wp)
     to minimize the hinge loss between the observed targets in the dataset using dual optimization problem.
@@ -348,7 +389,9 @@ class SupportVectorClassifierDual:
         Which regularization to use (ridge or lasso)
     C -- float, default=1
         Inverse of regularization coefficient
-    eps -- float, default=0.0005
+    degree -- int, default=None
+        Defines maximum degree of generating polynomial features
+    eps -- float, default=0.005
         Convergence tolerance of optimization problem
     plot -- bool, default=False
         Draw result plot or not
@@ -359,13 +402,19 @@ class SupportVectorClassifierDual:
         Pipeline with support vector classifier model and scaler
     ...
     """
-    def __init__(self, reg='Ridge', C=1, eps=0.0005, plot=False):
+    def __init__(self, reg='Ridge', C=1, degree=None, eps=0.005, plot=False):
         if reg.lower() == 'ridge':
             self.reg = 'l2'
         elif reg.lower() == 'lasso':
             self.reg = 'l1'
         else:
             raise ValueError('Regularization must be one of {\'Ridge\', \'Lasso\'}')
+        if degree:
+            self.degree = degree
+            self.poly_features = preprocessing.PolynomialFeatures(degree=self.degree, include_bias=False)
+        else:
+            self.degree = None
+            self.poly_features = None
         self.C = C
         self.eps = eps
         self.pipeline = pipeline.Pipeline([('Scaler', preprocessing.StandardScaler()),
@@ -388,19 +437,24 @@ class SupportVectorClassifierDual:
         -------
         self -- object, fitted model
         """
+        if self.poly_features:
+            X = self.poly_features.fit_transform(X)
         self.pipeline.fit(X, y)
         if self.plot:
             self.make_plot(X[:, :2], y)
         return self.pipeline
 
     def make_plot(self, X, y, grid_step=.01):
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=(14, 9))
         plt.scatter(X[y == 1, 0], X[y == 1, 1], c='green', marker='+', label='class 1')
         plt.scatter(X[y == 0, 0], X[y == 0, 1], c='red', marker='D', label='class 0')
         plt.xlabel("Feature 1")
         plt.ylabel("Feature 2")
         plt.title('Classification decision plane')
-        plt.legend()
+        legend_elements = [patches.Patch(facecolor='green', edgecolor='green', label='Class 1'),
+                           patches.Patch(facecolor='red', edgecolor='red', label='Class 2'),
+                           patches.Patch(facecolor='black', edgecolor='black', label='Separating plane')]
+        plt.legend(handles=legend_elements)
         x1_min, x1_max = X[:, 0].min() - .1, X[:, 0].max() + .1
         x2_min, x2_max = X[:, 1].min() - .1, X[:, 1].max() + .1
         xx, yy = np.meshgrid(np.arange(x1_min, x1_max, grid_step),
@@ -423,6 +477,8 @@ class SupportVectorClassifierDual:
         -------
         y -- array, array of predictions of regression model
         """
+        if self.poly_features:
+            X = self.poly_features.transform(X)
         return self.pipeline.predict(X)
 
     def __str__(self):
