82a8150b702d06e7b904cec6e4dc54504e8de8fd
Timofey Lashukov <tlashukov@gmail.com>
2022-06-21 21:40:20 +0300

    Doc string library

--- coreml/__init__.py
+++ coreml/__init__.py
@@ -1,2 +1,20 @@
+"""
+Machine learning optimization methods and actual models (with additional plotting)
+This library contains structure:
+Check https://github.com/M1croZavr/ML_opt
+
+NAME
+    coreml
+
+PACKAGE CONTENTS
+    classification (package)
+    gradients (package)
+    integerprogramming (package)
+    interiorpoint (package)
+    onedimensionaloptimization (package)
+    regression (package)
+    stochasticoptimization (package)
+    twovarextremas (package)
+"""
 from . import gradients, integerprogramming, interiorpoint, onedimensionaloptimization, regression, \
               stochasticoptimization, twovarextremas, classification
