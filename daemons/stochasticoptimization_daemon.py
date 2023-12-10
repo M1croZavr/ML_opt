8f9f49202f85ab691da62bc76d3cdda6656e86c5
M1croZavr <tlashukov@gmail.com>
2023-12-10 21:39:57 +0300

    Daemon added

--- daemons/stochasticoptimization_daemon.py
+++ daemons/stochasticoptimization_daemon.py
@@ -1,11 +1,32 @@
 import argparse
 import logging
+import signal
 import time
 
 import pandas as pd
 from sklearn import metrics
 
 from coreml.stochasticoptimization import models
+
+
+class SignalHandler:
+    shutdown = False
+
+    def __init__(self):
+        # Interrupt from keyboard (CTRL + C).
+        signal.signal(signal.SIGINT, self.request_shutdown)
+        # Termination signal.
+        signal.signal(signal.SIGTERM, self.request_shutdown)
+
+    def request_shutdown(self, *args):
+        print('Shutdown was requested')
+        self.shutdown = True
+
+    def can_run(self):
+        return not self.shutdown
+
+
+signal_handler = SignalHandler()
 
 urls = [
     'https://raw.githubusercontent.com/esokolov/'
@@ -41,14 +62,14 @@ logging.basicConfig(
 
 if __name__ == '__main__':
     # Simulation of model training and inference
-    args = arg_parser.parse_args()
-    while True:
+    parsed_args = arg_parser.parse_args()
+    while signal_handler.can_run():
         for url in urls:
             df = pd.read_csv(url)
             sgd_svm = models.StochasticSVM(
-                lr=args.learning_rate,
-                degree=args.degree,
-                C=args.regularization
+                lr=parsed_args.learning_rate,
+                degree=parsed_args.degree,
+                C=parsed_args.regularization
             )
             X = df.iloc[:, :-1].values
             y = df.iloc[:, -1].values
